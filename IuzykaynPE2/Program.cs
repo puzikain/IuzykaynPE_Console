d7c008863a112afa10515f1f4d4be3b40dfa9335
Iuzykayn P.E. <p.uzikain@mail.ru>
2026-04-06 17:26:02 +0300

    Add gitignore

---
+++ IuzykaynPE2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IuzykaynPE2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
